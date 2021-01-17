a9e8a01ba7a64352cc46a98cca75a2e03bc4bac1
AMIT VERMA <amit.verma@softobotics.com>
2021-01-17 18:27:18 +0530

    apkey api dene

---
+++ src/app/services/api-key.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiKeyService {
+
+  private apiPath: string;
+  private apikeys: string;
+
+  constructor(private httpClient: HttpClient) { 
+    const env: any = environment;
+    this.apiPath = env.paths.api;
+    this.apikeys = 'apikey';
+  }
+
+  newApiKey( sendApiKey ){
+    return this.httpClient.put<object>(`${this.apiPath}/${this.apikeys}/`, sendApiKey)
+  }
+}
