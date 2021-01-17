9650ce2b41d146a6a7a69f7a5a81cf499cf3240e
AMIT VERMA <amit.verma@softobotics.com>
2021-01-17 20:38:13 +0530

    apikey update from database

--- routes/business/searching.js
+++ routes/business/searching.js
@@ -1,20 +1,27 @@
 const express = require('express');
 const router = express.Router();
 var request = require('request');
+const Apikey = require('../../models').Apikay;
 
 
 
 router.post('/', async (req, res, next) => {
-    var keyword = req.body.keyword;
-    var radius = req.body.radius*1000;
-    var option = {
-        method: 'GET',
-        url: 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=12.9729391,77.6294794&radius='+radius+'&keyword='+keyword+'&key=AIzaSyAhhL0iW8nLYyCPsAHGv2Wj9CIKOx9TiDk',
-        json: true
-     };
-     request(option, function (error, response, body) {
-        res.json({ success: true, data: body.results });
-     })
+   Apikey.findOne(
+      {where:{apikeyId:1}},
+      {attributes: [  'apikey' ]}
+   ).then((apikey)=>{
+      var keyword = req.body.keyword;
+      var radius = req.body.radius*1000;
+      var option = {
+          method: 'GET',
+          url: 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=12.9729391,77.6294794&radius='+radius+'&keyword='+keyword+'&key='+apikey.apikey,
+          json: true
+       };
+       request(option, function (error, response, body) {
+          res.json({ success: true, data: body.results });
+       })
+   })
+   
 })
   
 
